e8679203464e23189d78851cf038a9b25b2d6e86
anekatdj <60734487+anekatdj@users.noreply.github.com>
2021-03-10 09:28:48 +0100

    klasser

---
+++ ChargingCabinet/CurrentEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ChargingCabinet
+{
+    public class CurrentEventArgs : EventArgs
+    {
+        // Value in mA (milliAmpere)
+        public double Current { set; get; }
+    }
+}
---
+++ ChargingCabinet/IUsbCharger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ChargingCabinet
+{
+    public interface IUsbCharger
+    {
+        // Event triggered on new current value
+        event EventHandler<CurrentEventArgs> CurrentValueEvent;
+
+        // Direct access to the current current value
+        double CurrentValue { get; }
+
+        // Require connection status of the phone
+        bool Connected { get; }
+
+        // Start charging
+        void StartCharge();
+
+        // Stop charging
+        void StopCharge();
+    }
+}
