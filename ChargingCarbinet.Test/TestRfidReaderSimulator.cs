9ccb0f76e6d31aea7276bc891bb35efe702f9404
laurabirchx <57993207+laurabirchx@users.noreply.github.com>
2021-03-23 16:14:07 +0100

    test til rfidreadersimulator påbegyndt

--- ChargingCarbinet.Test/TestRfidReaderSimulator.cs
+++ ChargingCarbinet.Test/TestRfidReaderSimulator.cs
@@ -1,12 +1,19 @@
+using ChargingCabinet.Interfaces;
+using ChargingCabinet.Simulators;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace ChargingCarbinet.Test
 {
     public class Tests
     {
+        private IStationControl _stationControl;
+        private IRfidReaderSimulator _uut;
         [SetUp]
         public void Setup()
         {
+            _stationControl = Substitute.For<IStationControl>();
+            _uut = new RfidReaderSimulator(_stationControl);
         }
 
         [Test]
