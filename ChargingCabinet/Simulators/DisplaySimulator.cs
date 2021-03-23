debf129851a7d3b498eaf0e7d53c3b7fb3600444
belindaabusk <au615813@post.au.dk>
2021-03-23 20:24:43 +0100

    Nye messages svarende til dem i state machine diagrammet i DisplaySimulator

--- ChargingCabinet/Simulators/DisplaySimulator.cs
+++ ChargingCabinet/Simulators/DisplaySimulator.cs
@@ -18,22 +18,22 @@ namespace ChargingCabinet.Simulators
 
         public void ShowPhoneChargingMessage()
         {
-            Console.WriteLine("Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op.");
+            Console.WriteLine("Ladeskab optaget");
         }
 
         public void ShowConnectionErrorMessage()
         {
-            Console.WriteLine("Din telefon er ikke ordentlig tilsluttet. Prøv igen.");
+            Console.WriteLine("Tilslutningsfejl");
         }
 
         public void ShowRfidErrorMessage()
         {
-            Console.WriteLine("Forkert RFID tag");
+            Console.WriteLine("RFID fejl");
         }
 
         public void ShowTakePhoneAndCloseDoorMessage()
         {
-            Console.WriteLine("Tag din telefon ud af skabet og luk døren");
+            Console.WriteLine("Fjern telefon");
         }
 
         public void ShowFullyChargedMessage()
