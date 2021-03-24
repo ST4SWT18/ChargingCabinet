650c25189607b1803334e1fe6d364e88f385f92f
laurabirchx <57993207+laurabirchx@users.noreply.github.com>
2021-03-24 15:35:33 +0100

    test af om tests af logfile kører på jenkins

--- ChargingCarbinet.Test/TestLogFileSimulator.cs
+++ ChargingCarbinet.Test/TestLogFileSimulator.cs
@@ -18,23 +18,34 @@ namespace ChargingCarbinet.UnitTests
             _uut = new LogFileSimulator();
         }
 
+        [TestCase(50, 2)]
+        public void LogDoorLocked_WritesToFileWithID_LengthIsLargerThanOne(int id, int numberOfNumbers)
+        {
+            _uut.LogDoorLocked(id);
+            var fileText = File.ReadLines("logfile.txt");
+            Assert.IsTrue(fileText.ToString().Length >= numberOfNumbers);
+            
+        }
+    }
+
+
+    public class AnotherTestLogFileSimulator
+    {
+        private LogFileSimulator _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new LogFileSimulator();
+        }
+
         [TestCase(50)]
-        public void Test1(int id)
+        public void Test2(int id)
         {
             var Id = id;
-            _uut.LogDoorLocked(Id);
+            _uut.LogDoorUnlocked(Id);
             var fileText = File.ReadLines("logfile.txt");
             Assert.IsTrue(fileText.ToString().Length > 1);
         }
-
-
-        //[TestCase(50)]
-        //public void Test2(int id)
-        //{
-        //    var Id = id;
-        //    _uut.LogDoorUnlocked(Id);
-        //    var fileText = File.ReadLines("logfile.txt");
-        //    Assert.IsTrue(fileText.ToString().Length > 1);
-        //}
     }
 }
