4bb20bab30650468dfb0fe081e4abe34f56a2101
laurabirchx <57993207+laurabirchx@users.noreply.github.com>
2021-03-24 15:56:58 +0100

    nyt forsøg igen igen igen

--- ChargingCabinet1/Simulators/LogFileSimulator.cs
+++ ChargingCabinet1/Simulators/LogFileSimulator.cs
@@ -6,11 +6,12 @@ namespace ChargingCabinet.Interfaces
 {
     public class LogFileSimulator : ILogFileSimulator
     {
-        private string logFile = "logfile.txt"; // Navnet på systemets log-fil
+        private string logFile1 = "logfile1.txt"; // Navnet på systemets log-fil
+        private string logFile2 = "logfile2.txt"; // Navnet på systemets log-fil
 
         public void LogDoorLocked(int Id)
         {
-            using (var writer = File.AppendText(logFile))
+            using (var writer = File.AppendText(logFile1))
             {
                 writer.WriteLine(DateTime.Now + ": Skab låst med RFID: {0}", Id);
             }
@@ -18,7 +19,7 @@ namespace ChargingCabinet.Interfaces
 
         public void LogDoorUnlocked(int Id)
         {
-            using (var writer = File.AppendText(logFile))
+            using (var writer = File.AppendText(logFile2))
             {
                 writer.WriteLine(DateTime.Now + ": Skab låst op med RFID: {0}", Id);
             }
