fec6d98d4d68ba5389c39b9ede79609d0413c0f1
laurabirchx <57993207+laurabirchx@users.noreply.github.com>
2021-03-24 09:15:23 +0100

    unit tests til testlogfileismulator

--- ChargingCarbinet.Test/TestLogFileSimulator.cs
+++ ChargingCarbinet.Test/TestLogFileSimulator.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using ChargingCabinet.Interfaces;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 
 namespace ChargingCarbinet.UnitTests
 {
@@ -16,10 +18,23 @@ namespace ChargingCarbinet.UnitTests
             _uut = new LogFileSimulator();
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(50)]
+        public void Test1(int id)
         {
-            Assert.Pass();
+            var Id = id;
+            _uut.LogDoorLocked(Id);
+            var fileText = File.ReadLines("logfile.txt");
+            Assert.IsTrue(fileText.ToString().Length > 1);
+        }
+
+
+        [TestCase(50)]
+        public void Test2(int id)
+        {
+            var Id = id;
+            _uut.LogDoorUnlocked(Id);
+            var fileText = File.ReadLines("logfile.txt");
+            Assert.IsTrue(fileText.ToString().Length > 1);
         }
     }
 }
